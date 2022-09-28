3a4b34942c22ae58d410dc297c220f72b820136e
argon <argonglach@gmail.com>
2022-09-28 17:13:05 -0400

    heroky push AGAIN

--- app/models/trades.py
+++ app/models/trades.py
@@ -1,24 +1,24 @@
-from .db import db
-from datetime import datetime
+# from .db import db
+# from datetime import datetime
 
 
-class Holdings(db.Model):
-    __tablename__ = "holdings"
+# class Holdings(db.Model):
+#     __tablename__ = "holdings"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolios.id"), nullable=False)
-    token_id = db.Column(db.Integer, nullable=False)
-    token_name = db.Column(db.String(255), nullable=False)
-    token_amount = db.Column(db.Integer, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+#     portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolios.id"), nullable=False)
+#     token_id = db.Column(db.Integer, nullable=False)
+#     token_name = db.Column(db.String(255), nullable=False)
+#     token_amount = db.Column(db.Integer, nullable=False)
 
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'portfolio_id': self.portfolio_id,
-            'token_id': self.token_id,
-            'token_name': self.token_name,
-            'token_amount': self.token_amount
-        }
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'user_id': self.user_id,
+#             'portfolio_id': self.portfolio_id,
+#             'token_id': self.token_id,
+#             'token_name': self.token_name,
+#             'token_amount': self.token_amount
+#         }
