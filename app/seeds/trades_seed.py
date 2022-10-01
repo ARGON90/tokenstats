34cd5a5445ca85bc1dc03d0b8facd4efe7358426
argon <argonglach@gmail.com>
2022-09-30 23:50:41 -0400

    added larger token list, changed seed

--- app/seeds/trades_seed.py
+++ app/seeds/trades_seed.py
@@ -5,10 +5,10 @@ from app.models.trades import Trade
 def seed_trades():
 
     demo_one = Trade(
-        user_id=1, portfolio_id=1, token_id=1, token_name='bitcoin', trade_price=20000, amount_traded=0.5, buy='buy', total_cost=10000
+        user_id=1, portfolio_id=1, token_id=1, token_name='bitcoin', trade_price=20000, amount_traded=0.5, buy='buy', total_cost=1000
     )
     demo_two = Trade(
-        user_id=1, portfolio_id=2, token_id=2, token_name='ethereum', trade_price=1500, amount_traded=0.5, buy='sell', total_cost=750
+        user_id=1, portfolio_id=2, token_id=2, token_name='ethereum', trade_price=1500, amount_traded=50, buy='buy', total_cost=400
     )
 
 
